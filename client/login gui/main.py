165258e43c289d96bdb7a88ddced02114bf64c36
youssef samaan <youssef.samaan@mail.mcgill.ca>
2022-10-27 11:55:15 -0400

    Added constants

--- client/login gui/main.py
+++ client/login gui/main.py
@@ -4,12 +4,16 @@ from login import login
 HEIGHT = 750
 WIDTH = 900
 GREY = (57, 57, 57)
+WHITE = (255,255,255)
+LIGHT_GREY = (99, 99, 99)
+LIGHT_BLUE = "lightskyblue3"
+FPS = 60
 
 pygame.init()
 pygame.display.set_caption('Splendor')
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 
 splendor_text = pygame.image.load('client\login gui\splendor-title.png')
 splendor_text = pygame.transform.scale(splendor_text, (500,200))
@@ -24,14 +28,14 @@ username_input_rect = pygame.Rect((150, 350, 200, 35)) # pos_x, pos_x, width, he
 password_input_rect = pygame.Rect((150, 450, 200, 35))
 
 login_rect = pygame.Rect((350, 600, 200, 70))
-login_text = base_font.render('Login', True, (255,255,255))
+login_text = base_font.render('Login', True, WHITE)
 
-username_text_display = base_font.render('Username', True, (255,255,255))
-password_text_display = base_font.render('Password', True, (255,255,255))
+username_text_display = base_font.render('Username', True, WHITE)
+password_text_display = base_font.render('Password', True, WHITE)
 
 
-color_active = pygame.Color("lightskyblue3")
-color_passive = pygame.Color(99, 99, 99)
+color_active = pygame.Color(LIGHT_BLUE)
+color_passive = pygame.Color(LIGHT_GREY)
 
 username_color = color_passive
 password_color = color_passive
@@ -89,10 +93,10 @@ while True:
     pygame.draw.rect(screen, password_color, password_input_rect, 3)
     pygame.draw.rect(screen, (0, 204, 0), login_rect)
 
-    username_text_surface = base_font.render(username_text, True, (255,255,255))
+    username_text_surface = base_font.render(username_text, True, WHITE)
     screen.blit(username_text_surface, (username_input_rect.x + 5, username_input_rect.y + 5))
 
-    password_text_surface = base_font.render(password_text, True, (255,255,255))
+    password_text_surface = base_font.render(password_text, True, WHITE)
     screen.blit(password_text_surface, (password_input_rect.x + 5, password_input_rect.y + 5))
 
     screen.blit(login_text, (420,625))
@@ -103,4 +107,4 @@ while True:
     password_input_rect.w = max(600, password_text_surface.get_width() + 10)
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(FPS)
