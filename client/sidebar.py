9e36a3377f4eb79b856864efdac63625b76b1908
youssef samaan <youssef.samaan@mail.mcgill.ca>
2022-10-30 16:51:48 -0400

    fixed the names to have consistent naming convention and started to add some changes

--- client/sidebar.py
+++ client/sidebar.py
@@ -8,45 +8,44 @@ from utils import *
 @Singleton
 class Sidebar:
     def __init__(self, screenWidth, screenHeight):
-                # [0] is width, [1] is height
+        # [0] is width, [1] is height
         self.card_size = Card.get_card_size(Board.instance())
         self.noble_size = Noble.get_card_size()
-        # self.sidebarImage = pygame.image.load('sprites/sidebar.png')
-        #self.width = min(screenWidth/2, 3*self.card_size[0])
         self.width = min(screenWidth/2, self.card_size[0])
         self.height = 10000 #min(screenHeight, 800)
-        # self.sidebarImage = pygame.transform.scale(self.sidebarImage, (self.width, self.height))
-        self.sidebarRect = pygame.Rect(0, 0, self.width, self.height)
+        self.sidebar_rect = pygame.Rect(0, 0, self.width, self.height)
         # centered on the right side of the screen
-        self.sidebarRect.center = (self.card_size[0]+self.noble_size[0]/2, screenHeight / 2)
-        #self.sidebarRect.center = (self.card_size[0]/2, screenHeight / 2)
+
+        # ???????????
+        self.sidebar_rect_center = (self.card_size[0]+self.noble_size[0]/2, screenHeight / 2)
+
         self.cards = {}
         self.reserved_cards = {}
         self.nobles = {}
-
-        # self.last_position_card = (0,0)
-        # self.last_position_noble = (self.card_size[0],0)
-        # self.last_position_reserved = (self.card_size[0]+self.noble_size[0],0) 
+ 
         self.last_position_card = (0,self.card_size[0])
         self.last_position_noble = (0,self.card_size[0])
         self.last_position_reserved = (0,self.card_size[0])
         
-        self.currentDisplay = 0
-        self.buttonRect1 = pygame.Rect(0, 0, self.card_size[0]/2, self.card_size[1] / 4)
-        self.buttonRect2 = pygame.Rect( self.card_size[0]/2, 0, self.card_size[0]/2, self.card_size[1] / 4)
+        self.current_display = 0
+        self.reserve_button = pygame.Rect(0, 10, self.width/3, self.card_size[1] / 4)
+        self.nobles_button = pygame.Rect(self.width/3, 10, 
+                                            self.card_size[0]/2, self.card_size[1] / 4)
+        self.bought_button = pygame.Rect(self.width*2/3, 10,
+                                            self.card_size[0]/2, self.card_size[1] / 4)
         
 
     def toggle1(self):
-        if self.currentDisplay == 0 or self.currentDisplay == 2:
-            self.currentDisplay = 1
+        if self.current_display == 0 or self.current_display == 2:
+            self.current_display = 1
         else:
-            self.currentDisplay = 0
+            self.current_display = 0
     
     def toggle2(self):
-        if self.currentDisplay == 0 or self.currentDisplay == 1:
-            self.currentDisplay = 2
+        if self.current_display == 0 or self.current_display == 1:
+            self.current_display = 2
         else:
-            self.currentDisplay = 0
+            self.current_display = 0
 
 
     def display(self, screen):
@@ -54,14 +53,14 @@ class Sidebar:
         # screen.blit(self.sidebarImage, self.sidebarRect)
         
         # draw dummy color background
-        pygame.draw.rect(screen, (0,0,0,0), self.sidebarRect)
-        if self.currentDisplay == 0:
+        pygame.draw.rect(screen, (0,0,0,0), self.sidebar_rect)
+        if self.current_display == 0:
 
             for card in self.cards:
                 card.draw(screen, self.cards[card][0], self.cards[card][1])
             self.draw_nobles_button(screen)
             self.draw_reserved_button(screen)
-        elif self.currentDisplay == 1:
+        elif self.current_display == 1:
 
             for noble in self.nobles:
                 noble.draw(screen, self.nobles[noble][0], self.nobles[noble][1])
@@ -80,7 +79,7 @@ class Sidebar:
         :param surface:
         :return:
         """
-        buy_button = button('Nobles', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=GREEN)
+        buy_button = button('Nobles', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
         x = 0
         y =  0
         surface.blit(buy_button, (x, y))
@@ -96,7 +95,7 @@ class Sidebar:
         :return:
         """
         # this is a toggle button that switches between bought and reserved
-        buy_button = button('Reserved', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=GREEN)
+        buy_button = button('Reserved', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
         x = self.card_size[0]/2
         y = 0
         surface.blit(buy_button, (x, y))
@@ -112,7 +111,7 @@ class Sidebar:
         :return:
         """
         # this is a toggle button that switches between bought and reserved
-        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=GREEN)
+        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
         x = self.card_size[0]/2
         y =  0
         surface.blit(buy_button, (x, y))
@@ -128,7 +127,7 @@ class Sidebar:
         :return:
         """
         # this is a toggle button that switches between bought and reserved
-        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=GREEN)
+        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
         x = 0
         y =  0
         surface.blit(buy_button, (x, y))
@@ -150,7 +149,7 @@ class Sidebar:
         self.last_position_reserved = (self.last_position_reserved[0], self.last_position_reserved[1]+self.card_size[1])
         
     def update_positions(self, amount):
-        # updating the last valuse for new cards
+        # updating the last values for new cards
         self.last_position_card = (self.last_position_card[0], self.last_position_card[1] + amount)
         self.last_position_noble = (self.last_position_noble[0], self.last_position_noble[1] + amount)
         self.last_position_reserved =(self.last_position_reserved[0], self.last_position_reserved[1] + amount)
@@ -176,28 +175,28 @@ class Sidebar:
         #     self.update_positions(direction)
         #     self.sidebarRect.move_ip(0, direction)
         self.update_positions(direction)
-        self.sidebarRect.move_ip(0, direction)
+        self.sidebar_rect.move_ip(0, direction)
     
-    def getRect(self):
+    def get_rect(self):
         return self.sidebarRect
 
-    def getX(self):
+    def get_x(self):
         return self.sidebarRect.x
     
-    def getY(self):
+    def get_y(self):
         return self.sidebarRect.y
     
-    def getWidth(self):
+    def get_width(self):
         return self.width
 
-    def getHeight(self):
+    def get_height(self):
         return self.height
     
-    def getCenter(self):
+    def get_center(self):
         return self.sidebarRect.center
     
-    def isClickedToggle(self, mousePos):
-        if self.buttonRect1.collidepoint(mousePos) :
+    def is_clicked_toggle(self, mousePos):
+        if self.reserve_button.collidepoint(mousePos) :
             return 1
-        if self.buttonRect2.collidepoint(mousePos):
+        if self.nobles_button.collidepoint(mousePos):
             return 2
