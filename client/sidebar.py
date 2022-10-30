90fd4d6725ef9a32a9f50bd20f13ad71ffe1108f
youssef samaan <youssef.samaan@mail.mcgill.ca>
2022-10-30 19:42:08 -0400

    3 tabs + changed color

--- client/sidebar.py
+++ client/sidebar.py
@@ -7,17 +7,13 @@ from utils import *
 
 @Singleton
 class Sidebar:
-    def __init__(self, screenWidth, screenHeight):
+    def __init__(self, screen_width, screen_height):
         # [0] is width, [1] is height
         self.card_size = Card.get_card_size(Board.instance())
         self.noble_size = Noble.get_card_size()
-        self.width = min(screenWidth/2, self.card_size[0])
+        self.width = screen_width  - Board.instance().get_rect().width
         self.height = 10000 #min(screenHeight, 800)
         self.sidebar_rect = pygame.Rect(0, 0, self.width, self.height)
-        # centered on the right side of the screen
-
-        # ???????????
-        self.sidebar_rect_center = (self.card_size[0]+self.noble_size[0]/2, screenHeight / 2)
 
         self.cards = {}
         self.reserved_cards = {}
@@ -28,59 +24,60 @@ class Sidebar:
         self.last_position_reserved = (0,self.card_size[0])
         
         self.current_display = 0
-        self.reserve_button = pygame.Rect(0, 10, self.width/3, self.card_size[1] / 4)
-        self.nobles_button = pygame.Rect(self.width/3, 10, 
-                                            self.card_size[0]/2, self.card_size[1] / 4)
-        self.bought_button = pygame.Rect(self.width*2/3, 10,
-                                            self.card_size[0]/2, self.card_size[1] / 4)
+        self.bought_button = pygame.Rect(0, 0,
+                                            self.width/3, self.card_size[1] / 4)
+        self.nobles_button = pygame.Rect(self.width/3, 0, 
+                                            self.width/3, self.card_size[1] / 4)
+        self.reserve_button = pygame.Rect(self.width*2/3, 0, 
+                                            self.width/3, self.card_size[1] / 4)
+        
+        self.display_color_active = LIGHT_BLUE
         
 
-    def toggle1(self):
-        if self.current_display == 0 or self.current_display == 2:
-            self.current_display = 1
-        else:
-            self.current_display = 0
-    
-    def toggle2(self):
-        if self.current_display == 0 or self.current_display == 1:
-            self.current_display = 2
-        else:
-            self.current_display = 0
+    def toggle(self, num):
+        if type(num) !=  int:
+            return
+        elif -1 < num < 4:
+            self.current_display = num
 
 
     def display(self, screen):
-        """"""
-        # screen.blit(self.sidebarImage, self.sidebarRect)
-        
         # draw dummy color background
         pygame.draw.rect(screen, (0,0,0,0), self.sidebar_rect)
         if self.current_display == 0:
-
             for card in self.cards:
-                card.draw(screen, self.cards[card][0], self.cards[card][1])
-            self.draw_nobles_button(screen)
-            self.draw_reserved_button(screen)
+                card.draw(screen, self.cards[card][0]*1.5, self.cards[card][1]*1.5)
         elif self.current_display == 1:
-
             for noble in self.nobles:
                 noble.draw(screen, self.nobles[noble][0], self.nobles[noble][1])
-            self.draw_bought_button2(screen)
-            self.draw_reserved_button(screen)
         else:
-
             for reserve_card in self.reserved_cards:
                 reserve_card.draw(screen, self.reserved_cards[reserve_card][0], self.reserved_cards[reserve_card][1])   
+
+        self.draw_buttons(screen)
+
+    def draw_buttons(self, screen):
+        if self.current_display == 1:
+            self.draw_nobles_button(screen, self.display_color_active)
+            self.draw_reserved_button(screen)
             self.draw_bought_button(screen)
+        elif self.current_display == 2:
             self.draw_nobles_button(screen)
-            
-    def draw_nobles_button(self, surface: pygame.Surface):
+            self.draw_reserved_button(screen, self.display_color_active)
+            self.draw_bought_button(screen)
+        else:
+            self.draw_nobles_button(screen)
+            self.draw_reserved_button(screen)
+            self.draw_bought_button(screen, self.display_color_active)
+                    
+    def draw_nobles_button(self, surface: pygame.Surface, color=LIGHT_GREY):
         """
         The x and y coordinates returned are relative to the surface.
         :param surface:
         :return:
         """
-        buy_button = button('Nobles', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
-        x = 0
+        buy_button = button('Nobles', width=self.width/3, height=self.card_size[1] / 4, color=color)
+        x = self.width/3
         y =  0
         surface.blit(buy_button, (x, y))
         button_rect = buy_button.get_rect()
@@ -88,15 +85,15 @@ class Sidebar:
         button_rect.y = y
         return button_rect
 
-    def draw_reserved_button(self, surface: pygame.Surface):
+    def draw_reserved_button(self, surface: pygame.Surface, color=LIGHT_GREY):
         """
         The x and y coordinates returned are relative to the surface.
         :param surface:
         :return:
         """
         # this is a toggle button that switches between bought and reserved
-        buy_button = button('Reserved', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
-        x = self.card_size[0]/2
+        buy_button = button('Reserved', width=self.width/3, height=self.card_size[1] / 4, color=color)
+        x = self.width*2/3
         y = 0
         surface.blit(buy_button, (x, y))
         button_rect = buy_button.get_rect()
@@ -104,30 +101,13 @@ class Sidebar:
         button_rect.y = y
         return button_rect
 
-    def draw_bought_button(self, surface: pygame.Surface):
+    def draw_bought_button(self, surface: pygame.Surface, color=LIGHT_GREY):
         """
         The x and y coordinates returned are relative to the surface.
         :param surface:
         :return:
         """
-        # this is a toggle button that switches between bought and reserved
-        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
-        x = self.card_size[0]/2
-        y =  0
-        surface.blit(buy_button, (x, y))
-        button_rect = buy_button.get_rect()
-        button_rect.x = x
-        button_rect.y = y
-        return button_rect
-    
-    def draw_bought_button2(self, surface: pygame.Surface):
-        """
-        The x and y coordinates returned are relative to the surface.
-        :param surface:
-        :return:
-        """
-        # this is a toggle button that switches between bought and reserved
-        buy_button = button('Bought', width=self.card_size[0]/2, height=self.card_size[1] / 4, color=LIGHT_GREY)
+        buy_button = button('Bought', width=self.width/3, height=self.card_size[1] / 4, color=color)
         x = 0
         y =  0
         surface.blit(buy_button, (x, y))
@@ -192,11 +172,10 @@ class Sidebar:
     def get_height(self):
         return self.height
     
-    def get_center(self):
-        return self.sidebarRect.center
-    
     def is_clicked_toggle(self, mousePos):
         if self.reserve_button.collidepoint(mousePos) :
-            return 1
-        if self.nobles_button.collidepoint(mousePos):
             return 2
+        elif self.nobles_button.collidepoint(mousePos):
+            return 1
+        elif self.bought_button.collidepoint(mousePos):
+            return 0
