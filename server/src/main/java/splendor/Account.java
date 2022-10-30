2917921a038349beb8397522c16c34726c0a3e3e
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2022-10-30 16:41:35 -0400

    optimize imports

--- server/src/main/java/splendor/Account.java
+++ server/src/main/java/splendor/Account.java
@@ -1,16 +1,13 @@
 package splendor;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Base64;
 
