bc0343d1af3d926b5cb23ea868b8273d9ad9fbef
youssef samaan <youssef.samaan@mail.mcgill.ca>
2022-11-27 18:57:01 -0500

    chanages for me.

--- client/main.py
+++ client/main.py
@@ -1,8 +1,8 @@
-from client.authenticator import Authenticator
+from authenticator import Authenticator
 from game import splendor
 from login import login
 
 if __name__ == '__main__':
     authenticator = Authenticator()
     login.login(authenticator)
-    splendor.play(authenticator=authenticator, game_id="7620377171362791498")
+    splendor.play(authenticator=authenticator, game_id="3859030649588294952")
