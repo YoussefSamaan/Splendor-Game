a3948e09af398dd1b6f66a6ecf8e09ce1dfd66d5
youssef samaan <youssef.samaan@mail.mcgill.ca>
2023-02-24 10:41:39 -0500

    added ActionType class and some refactoring

---
+++ server/src/main/java/splendor/controller/action/ActionType.java
@@ -0,0 +1,12 @@
+package splendor.controller.action;
+
+/**
+ * All actions that can be performed on a development card or noble by a player.
+ */
+public enum ActionType {
+  BUY,
+  RESERVE,
+  TAKE_NOBLE,
+  TAKE_TOKENS,
+  TAKE_CARD
+}
