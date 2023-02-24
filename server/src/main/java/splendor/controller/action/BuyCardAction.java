51f33b44fb3d10eb4f87172071424ab0bcdc7037
youssef samaan <youssef.samaan@mail.mcgill.ca>
2023-02-24 10:28:50 -0500

    Buy card action class

---
+++ server/src/main/java/splendor/controller/action/BuyCardAction.java
@@ -0,0 +1,61 @@
+package splendor.controller.action;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.InsufficientResourcesException;
+import splendor.model.game.Board;
+import splendor.model.game.SplendorGame;
+import splendor.model.game.card.DevelopmentCardI;
+import splendor.model.game.card.SplendorCard;
+import splendor.model.game.deck.SplendorDeck;
+import splendor.model.game.player.Player;
+import splendor.model.game.player.SplendorPlayer;
+
+/**
+ * buying a card action class.
+ */
+public class BuyCardAction extends CardAction {
+  private static CardType cardType = CardType.DevelopmentCard;
+
+  /**
+   * Creates a new card action.
+   *
+   * @param card the card.
+   */
+  protected BuyCardAction(SplendorCard card) {
+    super(CardActionType.BUY, card);
+  }
+
+
+  /**
+   * Generates the list of buy actions for the player.
+   *
+   * @param game   the current game that is being played.
+   * @param player the player we are generating actions for.
+   * @return all legal buy actions for the given player in the given game state.
+   */
+  public static List<Action> getLegalActions(SplendorGame game, SplendorPlayer player) {
+    List<Action> actions = new ArrayList<>();
+    for (SplendorDeck deck : game.getBoard().getDecks()) {
+      for (DevelopmentCardI card : deck.getFaceUpCards()) {
+        if (card != null && player.canAfford(card)) {
+          actions.add(new BuyCardAction(card));
+        }
+      }
+    }
+    return actions;
+  }
+
+
+  @Override
+  public void preformAction(Player player, Board board) {
+    try {
+      player.buyCard((DevelopmentCardI) this.getCard()); // should always work
+    } catch (InsufficientResourcesException e) {
+      throw new RuntimeException(e);
+    }
+    board.removeCard(this.getCard());
+  }
+
+
+}
