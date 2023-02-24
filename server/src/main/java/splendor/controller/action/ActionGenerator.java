a167e493bfd423017fbb2225d5853599fb9a953e
youssef samaan <youssef.samaan@mail.mcgill.ca>
2023-02-24 10:28:33 -0500

    added more logic to decide what type of actions we need to generate

---
+++ server/src/main/java/splendor/controller/action/ActionGenerator.java
@@ -0,0 +1,78 @@
+package splendor.controller.action;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import splendor.model.game.SplendorGame;
+import splendor.model.game.card.SpecialActions;
+import splendor.model.game.player.SplendorPlayer;
+
+/**
+ * Class responsible for generating all possible actions for a given game state.
+ */
+@Component
+public class ActionGenerator {
+  private final HashMap<Long, List<Action>> gameActions = new HashMap<>();
+
+  /**
+   * Returns all possible actions for a given game state.
+   *
+   * @param game the game
+   * @param player the player
+   * @return all possible actions for a given game state
+   */
+  public List<Action> generateActions(SplendorGame game, long gameId,  SplendorPlayer player) {
+    List<Action> actions = new ArrayList<>();
+    if (!game.isTurnPlayer(player)) {
+      return actions;
+    }
+    if (gameActions.containsKey(gameId)) {
+      // To check if the action was already generated for the player
+      return gameActions.get(gameId);
+    }
+    // TODO: ADD more actions
+    if (player.nextAction() == null) {
+      // add take tokens
+      actions.addAll(ReserveCardAction.getLegalActions(game, player));
+      actions.addAll(BuyCardAction.getLegalActions(game, player));
+    } else if (player.nextAction() == SpecialActions.TakeFreeLevel2) {
+      actions.addAll(TakeCardAction.getLegalActions(game, 2));
+    } else if (player.nextAction() == SpecialActions.TakeFreeLevel1) {
+      actions.addAll(TakeCardAction.getLegalActions(game, 1));
+    } else if (player.nextAction() == SpecialActions.ChooseNoble) {
+      actions.addAll(TakeNobleAction.getLegalActions(game, player, false));
+    } else {
+      actions.addAll(TakeNobleAction.getLegalActions(game, player, true));
+    }
+    gameActions.put(gameId, actions); // save the actions for later validation
+    return actions;
+  }
+
+  /**
+   * Returns a previously generated action.
+   *
+   * @param gameId the id of the game
+   * @param actionId the id of the action
+   * @return the action
+   * @throws InvalidAction if the action does not exist
+   */
+  public Action getGeneratedAction(long gameId, long actionId) throws InvalidAction {
+    if (!gameActions.containsKey(gameId)) {
+      throw new InvalidAction("Game id does not exist");
+    }
+    return gameActions.get(gameId).stream()
+        .filter(action -> action.getId() == actionId)
+        .findFirst()
+        .orElseThrow(() -> new InvalidAction("Action not found"));
+  }
+
+  /**
+   * Removes the action that has been executed.
+   *
+   * @param gameId the id of the game
+   */
+  public void removeActions(long gameId) {
+    gameActions.remove(gameId);
+  }
+}
