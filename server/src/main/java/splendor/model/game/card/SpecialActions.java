d695dda11d3f8b67d801cc60330ff2fd5e82f01a
youssef samaan <youssef.samaan@mail.mcgill.ca>
2023-02-24 10:32:12 -0500

    different special action that can be gained from buying red cards

---
+++ server/src/main/java/splendor/model/game/card/SpecialActions.java
@@ -0,0 +1,13 @@
+package splendor.model.game.card;
+
+/**
+ * Special actions that can be obtained from buying red cards.
+ */
+public enum SpecialActions {
+  TakeFreeLevel2,
+  TakeFreeLevel1,
+  Clone,
+  ChooseNoble,
+  ReserveNoble,
+  DoubleBonus // might need to add more
+}
