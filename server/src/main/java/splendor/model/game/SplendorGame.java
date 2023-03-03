96163cce6fcf70f1818ccb25d1a11522bec19fa7
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-03-03 17:00:49 -0500

    add check for turn player

--- server/src/main/java/splendor/model/game/SplendorGame.java
+++ server/src/main/java/splendor/model/game/SplendorGame.java
@@ -87,6 +87,8 @@ public class SplendorGame {
       throws InsufficientResourcesException {
     // No use of action data for now, system automatically decides tokens to use for payment
     Player player = getPlayer(username);
+    if (!isTurnPlayer(player)) { return; }
+
     action.preformAction(player, board);
 
     // add logic to next turn
