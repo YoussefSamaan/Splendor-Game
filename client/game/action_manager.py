45c6f6dcaf0411b075be450c4ee952d03c3b0706
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2023-03-05 15:18:35 -0500

    debug action list

--- client/game/action_manager.py
+++ client/game/action_manager.py
@@ -20,6 +20,8 @@ class ActionManager:
         self.actions = server_manager.get_actions(self.authenticator, self.game_id, player_name)
 
     def get_card_action_id(self, card: Card, player_name: str, action_type: Action) -> int:
+        
+
         if action_type == Action.CANCEL:
             return 0
         if player_name != self.last_updated_player:
@@ -31,8 +33,10 @@ class ActionManager:
                 return action["actionId"]
         return -1
 
-    # Given a Dict[Token,int], return the action id
+    # Given a Dict[Token,int], return the action id for taking tokens / returning tokens
     def get_token_action_id(self, token_selection: Dict[Token,int], player_name: str, action_type: Action) -> int:
+        print(self.actions)
+        
         if action_type == Action.CANCEL:
             return 0
         if player_name != self.last_updated_player:
@@ -51,7 +55,9 @@ class ActionManager:
                 and action["actionType"] == action_type.value:
                 return action["actionId"]
         return -2
-        
+    
+    def get_actions_json(self):
+        print(self.actions)
 
     def perform_action(self, action_id: int):
         server_manager.perform_action(self.authenticator, self.game_id, self.last_updated_player,
