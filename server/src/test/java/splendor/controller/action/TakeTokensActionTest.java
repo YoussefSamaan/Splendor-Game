9c78742f514389a87b640af815402f7340c7932e
XiaoyuJessieXu1 <xiaoyu.xu3@mail.mcgill.ca>
2023-04-04 12:00:42 -0400

    added some unit test

--- server/src/test/java/splendor/controller/action/TakeTokensActionTest.java
+++ server/src/test/java/splendor/controller/action/TakeTokensActionTest.java
@@ -1,8 +1,10 @@
 package splendor.controller.action;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import splendor.controller.lobbyservice.GameInfo;
@@ -10,7 +12,7 @@ import splendor.model.game.Color;
 import splendor.model.game.SplendorGame;
 import splendor.model.game.player.Inventory;
 import splendor.model.game.player.Player;
-
+ import splendor.model.game.Color;
 public class TakeTokensActionTest {
   ActionGenerator actionGenerator = new ActionGenerator();
   SplendorGame game;
@@ -56,6 +58,104 @@ public class TakeTokensActionTest {
       numOfTokens -= player1.getTokens().get(c);
     }
     assertTrue(numOfTokens == 1 || numOfTokens == 0);
+  }
+
+  @Test
+  public void performTakeOneTokenAction(){
+    Player[] testPlayers = {player1,player2};
+    GameInfo testGameInfo = new GameInfo("testServer","SplendorGameTest",testPlayers,"testSave");
+    game = new SplendorGame(testGameInfo);
+
+    try {
+      clearPlayerTokens(player1);
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    token.put(Color.BLUE, 1);
+
+    long gameId = 1;
+    TakeOneTokenAction tester = new TakeOneTokenAction(ActionType.TAKE_ONE_TOKEN,token);
+    tester.performAction(player1, game.getBoard());
+    assertEquals(true, player1.getTokens().get(Color.BLUE) == 1);
+  }
+
+  /***
+   * Sould be added 10 blue. then remove 2 blue, and added two blue
+   * buggy
+  @Test
+  public void performTakeTwoTokenAction(){
+    Player[] testPlayers = {player1,player2};
+    GameInfo testGameInfo = new GameInfo("testServer","SplendorGameTest",testPlayers,"testSave");
+    game = new SplendorGame(testGameInfo);
+
+    try {
+      clearPlayerTokens(player1);
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+    HashMap<Color, Integer> tenBlueToken = new HashMap<Color, Integer>();
+    tenBlueToken.put(Color.BLUE, 10);
+    player1.addTokens(tenBlueToken);
+
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    token.put(Color.BLUE, 2);
+
+    ReturnTokensAction return2Blue = new return
+    long gameId = 1;
+    TakeTokensAction tester = new TakeTokensAction(ActionType.TAKE_TOKENS,token);
+    tester.performAction(player1, game.getBoard());
+    assertEquals(true, player1.getTokens().get(Color.BLUE) == 10);
+  }
+***/
+  @Test
+  public void performTakeThreeTokenAction(){
+    Player[] testPlayers = {player1,player2};
+    GameInfo testGameInfo = new GameInfo("testServer","SplendorGameTest",testPlayers,"testSave");
+    game = new SplendorGame(testGameInfo);
+
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    token.put(Color.BLUE, 3);
+
+    long gameId = 1;
+    TakeTokensAction tester = new TakeTokensAction(ActionType.TAKE_TOKENS,token);
+    tester.performAction(player1, game.getBoard());
+    assertEquals(true, player1.getTokens().get(Color.BLUE) == 3);
+  }
+
+  @Test
+  public void getLegalActions(){
+    Player[] testPlayers = {player1,player2};
+    GameInfo testGameInfo = new GameInfo("testServer","SplendorGameTest",testPlayers,"testSave");
+    game = new SplendorGame(testGameInfo);
+
+    try {
+      clearPlayerTokens(player1);
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    token.put(Color.BLUE, 1);
+
+    long gameId = 1;
+    TakeOneTokenAction tester = new TakeOneTokenAction(ActionType.TAKE_ONE_TOKEN,token);
+    assertEquals(true, tester.getLegalActions(game).size() > 0);
+  }
+
+  @Test
+  public void getActionType(){
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    TakeOneTokenAction tester = new TakeOneTokenAction(ActionType.TAKE_ONE_TOKEN,token);
+    assertEquals(true, tester.getActionType() == ActionType.TAKE_ONE_TOKEN);
+  }
+
+  @Test
+  public void getActionId(){
+    HashMap<Color, Integer> token = new HashMap<Color, Integer>();
+    TakeOneTokenAction tester = new TakeOneTokenAction(ActionType.TAKE_ONE_TOKEN,token);
+    assertTrue(tester.getId() > 0);
   }
 
 //  @Test // not working for some reason
