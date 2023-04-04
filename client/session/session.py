f3806ffde6791b4c53722ac91a10bdcf8a26a449
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2023-04-04 09:57:05 -0400

    Update session.py

--- client/session/session.py
+++ client/session/session.py
@@ -269,7 +269,7 @@ def session(authenticator :Authenticator) -> int:
         clickable_buttons :List[Button] = []
 
         def parse_type() -> str:
-            game_type = "Splendor"
+            game_type = "splendor"
             if trade_toggle.active:
                 game_type += "Traderoutes"
             if cities_toggle.active:
