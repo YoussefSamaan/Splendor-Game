82afedb18d3ac0092bf2fe92a642ca5adfaeb173
ivey kun <44329173+iveykun@users.noreply.github.com>
2023-04-05 19:03:57 -0400

    added dummy code for win condition

--- client/game/splendor.py
+++ client/game/splendor.py
@@ -42,6 +42,7 @@ cascade = False
 TRADING_POST_ENABLED = False 
 CITIES_ENABLED = False
 EXIT = False
+is_won = False
 
 class IndividualTokenSelection:
     def __init__(self, token: Token, x_pos: int, y_pos: int) -> None:
@@ -156,6 +157,27 @@ def show_persistent_message(color=GREEN):
 
     flash_right_side(DISPLAYSURF, PERSISTENT_MESSAGE, color=color, opacity=255)
 
+def check_if_won():
+    # checks checkGameEnd in Board
+    # temp fake list of players
+    # TODO get the actual list of players from backend 
+    global is_won
+    lst = [Player.instance(id=0), Player.instance(id=1)] 
+    if is_won:
+        dim_screen(DISPLAYSURF)
+        if Player.instance(id=CURR_PLAYER) in lst and len(lst) == 1:
+            show_persistent_message("You won!", GREEN)
+            pygame.display.update()
+
+            return
+        elif Player.instance(id=CURR_PLAYER) in lst and len(lst) > 1:
+            show_persistent_message("You tied!", GREEN)
+            pygame.display.update()
+            return
+        else:
+            show_persistent_message("You lost!", RED)
+            pygame.display.update()
+            return
 
 def set_flash_message(text, color=GREEN, timer=5):
     global FLASH_MESSAGE, FLASH_TIMER, FLASH_START, FLASH_COLOR
@@ -881,6 +903,11 @@ def play(authenticator, game_id):
     update(authenticator, game_id)
     logged_in_user = authenticator.username
     while True:
+        if check_if_won():
+            for event in pygame.event.get():
+                #end game if click anything
+                pygame.quit()
+                sys.exit()
         if pygame.time.get_ticks() - last_update > 2000:
             last_update = pygame.time.get_ticks()
             # await async_update(authenticator, game_id)
